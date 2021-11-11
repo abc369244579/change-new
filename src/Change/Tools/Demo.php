2077a993b067fabb2c1588f236b417b1c6d9ded5
卢景昌 <lujingchang@sp.cc>
2021-11-11 14:21:12 +0800

    init

---
+++ src/Change/Tools/Demo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Change\Tools;
+
+class Demo
+{
+    public static function hello()
+    {
+        return 'hello';
+    }
+}
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use Change\Tools\Demo;
+
+$res = Demo::hello();
+var_dump($res);
